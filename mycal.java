b90228d399e9420ed424d0532c0cc38ec132538c
Geetha <154579135+Geethakanna@users.noreply.github.com>
2025-10-28 14:25:21 +0530

    Add files via upload

---
+++ mycal.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.invoke.*;
+import java.applet.*;
+public class mycal {
+   public static void main(String[] args) 
+   int price=10;
+   int value=price*10/100;
+   System.out.print(value);
+}
+ class Main{
+
+   int price=10;
+   double value=price*10/100;
+   double inter=value/0.1;
+   double layer = inter *price;
+   value= price - value;
+   System.out.print("discount"+value);
+   
+}
